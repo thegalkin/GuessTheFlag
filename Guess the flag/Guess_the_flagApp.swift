e742779b6dab345f5f1f24fd8c886962a9bb3623
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2020-10-21 13:27:26 +0300

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Никита Галкин on 21.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
